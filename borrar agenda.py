2f95e4a7a0c9f3098d6e850ed5dfdc29ff19640f
PacoBascon <94598639+PacoBascon@users.noreply.github.com>
2022-03-08 11:10:54 +0100

    Add files via upload

---
+++ borrar agenda.py
@@ -0,0 +1,8 @@
+def borrarnombre(): 
+elif opc == 3:
+        nombre = input("Nombre del contacto para borrar:")    
+        if nombre in agenda:
+            opc = input("Pulsa 's' si quieres borrarlo!!!
+            if opc == "s":
+                del agenda[nombre]
+                
