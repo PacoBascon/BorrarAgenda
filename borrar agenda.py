593b7743295927d287609763df1157b992bf4031
PacoBascon <94598639+PacoBascon@users.noreply.github.com>
2022-03-10 09:00:46 +0100

    Add files via upload

--- borrar agenda.py
+++ borrar agenda.py
@@ -1,3 +1,10 @@
+#Autor: Paco Bascón Valle
+#Version 3.10.2
+#Fecha 10/03/2022
+#Definimos borrarnombre
+#Buscamos un nombre en la agenda y si esta preguntamos que si queremos borrarlo
+
+
 def borrarnombre(nombre, agenda): 
         if nombre in agenda:
         opc = input("Pulsa 's' si quieres borrarlo!!!
